fc1d87d8829b30fac84ea1602c44e33d6b8636c3
BYGGG <133856676+BYGGG@users.noreply.github.com>
2024-05-26 20:20:06 +0100

    Divide the main.dart into 5 subpages and update the dependencies

---
+++ web_app/lib/data_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DataPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Data Page'),
+    );
+  }
+}
